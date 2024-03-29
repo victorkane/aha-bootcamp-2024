0652421207dd3a592edfec21c39febc40eec8486
victorkane <victorkane@gmail.com>
2024-03-29 20:14:02 -0300

    M03 build(dotenv): install and cofigure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import node from "@astrojs/node";
+import node from '@astrojs/node'
+import 'dotenv/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
-});
+    mode: 'standalone',
+  }),
+})
